26eef1146d6d8426892f657abb1659ce1d90ad87
feki_mohamed <medfeki33@gmail.com>
2024-12-08 20:56:17 +0100

    ajouter menu.css

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,41 +1,50 @@
 import React from 'react';
 import { Nav, Navbar, Container, Form, FormControl, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { FaBook, FaSearch } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
+import './Menu.css'; // Ajouter un fichier CSS personnalisé pour plus de style
 
 const Menu = () => {
   return (
-    <Navbar bg="primary" variant="dark" expand="lg" className="shadow-sm">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          <FaBook className="me-2" />
-          Gestion Commerciale
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/livres">Livres</Nav.Link>
-            <Nav.Link as={Link} to="/auteurs">Auteurs</Nav.Link>
-            <Nav.Link as={Link} to="/editeurs">Éditeurs</Nav.Link>
-            <Nav.Link as={Link} to="/specialistes">Spécialistes</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Rechercher un livre, auteur..."
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="success">
-              <FaSearch className="me-1" />
-              Chercher
-            </Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="menu-container">
+      {/* Barre de navigation */}
+      <Navbar expand="lg" className="navbar-custom shadow-sm">
+        <Container>
+          <Navbar.Brand as={Link} to="/" className="brand-custom">
+            Bibliothèque Digitale
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/livres" className="nav-link-custom">
+                Livres
+              </Nav.Link>
+              <Nav.Link as={Link} to="/auteurs" className="nav-link-custom">
+                Auteurs
+              </Nav.Link>
+              <Nav.Link as={Link} to="/editeurs" className="nav-link-custom">
+                Éditeurs
+              </Nav.Link>
+              <Nav.Link as={Link} to="/specialistes" className="nav-link-custom">
+                Spécialistes
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Rechercher..."
+                className="search-input"
+                aria-label="Search"
+              />
+              <Button variant="light" className="search-btn">
+                <FaSearch />
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
 export default Menu;
-
