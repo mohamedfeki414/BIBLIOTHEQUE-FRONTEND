72790328a7a6d2f93fe69d80cf62b4112dc3af0e
feki_mohamed <medfeki33@gmail.com>
2024-12-28 17:09:12 +0100

    modifier editauteur

--- src/components/auteurs/Editauteurs.jsx
+++ src/components/auteurs/Editauteurs.jsx
@@ -1,14 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { Form, Col, Row } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Form, Col, Row } from 'react-bootstrap'
-import { useNavigate, useParams } from 'react-router-dom'
 
 const Editauteurs = () => {
-  const [auteurs, SetAuteurs] = useState([]);
-  const navigate = useNavigate()
-  const { id } = useParams() 
+  const [auteurs, SetAuteurs] = useState({});
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-  const getLivres = async (id) => {
+  const getAuteur = async (id) => {
     try {
       const res = await axios.get(`http://localhost:5000/api/auteurs/${id}`);
       SetAuteurs(res.data);
@@ -18,64 +18,75 @@ const Editauteurs = () => {
   };
 
   useEffect(() => {
-    getLivres(id);
-  }, [id])
+    getAuteur(id);
+  }, [id]);
+
   const handleSave = async (e) => {
+    e.preventDefault();
     try {
-        e.preventDefault()
-      
-        await axios.put(`http://localhost:5000/api/auteurs/${id}`,auteurs)
-        .then(res=>{navigate("/auteurs") })
+      await axios.put(`http://localhost:5000/api/auteurs/${id}`, auteurs);
+      navigate("/auteurs");
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  };
+
   return (
     <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-    <Form>
+      <Form onSubmit={handleSave}>
         <Row>
-            <Form.Group as={Col} mb="6">
-                <Form.Label>Nom Auteur </Form.Label>
-                <Form.Control 
-                    type="text" 
-                    placeholder="Nom Auteur"
-                    value={auteurs.nomauteur}
-                    onChange={(e) =>  SetAuteurs({ ...auteurs, nomauteur: e.target.value })}
-                />
-            </Form.Group>
+          <Form.Group as={Col} className="mb-3">
+            <Form.Label>Nom Auteur</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Nom Auteur"
+              value={auteurs.nomauteur || ''}
+              onChange={(e) =>
+                SetAuteurs({ ...auteurs, nomauteur: e.target.value })
+              }
+            />
+          </Form.Group>
         </Row>
         <Row>
-            <Form.Group as={Col} mb="6">
-                <Form.Label>Email</Form.Label>
-                <Form.Control 
-                    type="text" 
-                    placeholder="email"
-                    value={auteurs.email}
-                    onChange={(e) =>  SetAuteurs({ ...auteurs, email: e.target.value })}
-                />
-            </Form.Group>
+          <Form.Group as={Col} className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              value={auteurs.email || ''}
+              onChange={(e) =>
+                SetAuteurs({ ...auteurs, email: e.target.value })
+              }
+            />
+          </Form.Group>
         </Row>
         <Row>
-            <Form.Group as={Col} mb="6">
-                <Form.Label>Num Tel</Form.Label>
-                <Form.Control 
-                    type="text" 
-                    placeholder="numtel"
-                    value={auteurs.numtel}
-                    onChange={(e) =>  SetAuteurs({ ...auteurs, numtel: e.target.value })}
-                />
-            </Form.Group>
+          <Form.Group as={Col} className="mb-3">
+            <Form.Label>Num Tel</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Num Tel"
+              value={auteurs.numtel || ''}
+              onChange={(e) =>
+                SetAuteurs({ ...auteurs, numtel: e.target.value })
+              }
+            />
+          </Form.Group>
         </Row>
-    </Form>
-    <button className="btn btn-success btn-sm" onClick={handleSave}>
-        <i className="fa-solid fa-floppy-disk"></i> Save
-    </button>
-    &nbsp;
-    <button className="btn btn-danger btn-sm" onClick={() => navigate("/auteurs")}>
-        <i className="fa-solid fa-person-walking-arrow-right"></i> Exit
-    </button>
-</div>
-  )
-}
+        <button className="btn btn-success btn-sm" type="submit">
+          <i className="fa-solid fa-floppy-disk"></i> Save
+        </button>
+        &nbsp;
+        <button
+          className="btn btn-danger btn-sm"
+          type="button"
+          onClick={() => navigate("/auteurs")}
+        >
+          <i className="fa-solid fa-person-walking-arrow-right"></i> Exit
+        </button>
+      </Form>
+    </div>
+  );
+};
 
-export default Editauteurs
+export default Editauteurs;
