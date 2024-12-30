c179ecca13b3dcc7090d3d217f3af3660a7c1974
feki_mohamed <medfeki33@gmail.com>
2024-12-30 21:19:35 +0100

    ajouter material-react-table

--- src/components/livres/Affichelivres.jsx
+++ src/components/livres/Affichelivres.jsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import React, { useMemo } from 'react';
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from 'material-react-table';
+import { Box } from '@mui/material'; 
+import Button from 'react-bootstrap/Button'; 
 
-const Affichelivres = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Affichelivres = ({livres}) => {
+    const columns = useMemo(
+        () => [
+            
+          {
+            accessorKey: 'isbn', // access nested data with dot notation
+            header: 'isbn',
+            size: 150,
+          },
+          {
+            accessorKey: 'titre',
+            header: 'titre',
+            size: 150,
+          },
+          {
+            accessorKey: 'annedition', // normal accessorKey
+            header: 'annedition',
+            size: 200,
+          },
+          { 
+         
+            accessorKey: 'prix', //normal accessorKey 
+     
+            header: 'prix', 
+     
+            size: 200, 
+     
+          },
+          { 
+         
+            accessorKey: 'qtestock', 
+     
+            header: 'State', 
+     
+            size: 150, 
+     
+          },
+          { 
+            accessorKey: 'couverture', //access nested data with dot notation 
+            header: 'Image', 
+            Cell: ({ cell}) => ( 
+            <Box 
+    
+    
+            sx={{ 
+            display: 'flex', 
+            alignItems: 'center', 
+            gap: '1rem', 
+            }} 
+            > 
+            <img 
+            alt="" 
+            height={100} 
+            src={cell.getValue()} 
+            loading="lazy" 
+            style={{ borderRadius: '20%' }} 
+            /> 
+            </Box>), 
+            }, 
+            { 
+                accessorKey: '_id', 
+                header: 'actions', 
+                size: 100, 
+                Cell: ({ cell, row }) => ( 
+                <div > 
+                <Button 
+                onClick={() => { 
+                console.log("modification ...") 
+                }} 
+                variant="warning" 
+                size="md" 
+                className="text-warning btn-link edit" 
+                > 
+                <i class="fa-solid fa-pen-to-square"></i> 
+                </Button> 
+                <Button 
+                onClick={(e) => { 
+                  deleteProduct(cell.row.original._id,cell.row.original.reference, 
+    e); 
+                }} 
+                variant="danger" 
+                size="md" 
+                className="text-danger btn-link delete" 
+                >
+                      <i className="fa fa-trash" /> 
+                </Button> 
+                </div> 
+                ), 
+                },
+    
+          
+        ],
+        []
+      );
+    
+      const table = useMaterialReactTable({
+        columns,
+        data: livres, // data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+      });
+    
+      return <MaterialReactTable table={table} />;
+    };
 
 export default Affichelivres
--- src/components/livres/Listlivrestable.jsx
+++ src/components/livres/Listlivrestable.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Affichearticles from './Affichearticles';
 
 const Listlivrestable = () => {
+    const[livres,setLivres]=useState([]); 
+     
+    const getLivres=async()=>{ 
+     await axios.get("http://localhost:5000/api/livres") 
+      .then(res=>{ 
+        setLivres(res.data) 
+      } 
+    ) 
+    .catch(error=>{ 
+      console.log(error) 
+    }) 
+  } 
+   
+  useEffect(() => { 
+      getLivres() 
+       },[]) 
+  //useAuth();
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/livres/${id}`);
+      setLivres(livres.filter((c) => c._id !== id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   return (
     <div>
-      
+       <Affichearticles livres={livres} handleDelete={handleDelete}/>
     </div>
   )
 }
