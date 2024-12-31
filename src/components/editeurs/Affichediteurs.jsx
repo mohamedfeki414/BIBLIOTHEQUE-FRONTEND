3166537eb9aaafbbb1ab031e2535297365e5c9e2
feki_mohamed <medfeki33@gmail.com>
2025-01-01 00:43:18 +0100

    .

--- src/components/editeurs/Affichediteurs.jsx
+++ src/components/editeurs/Affichediteurs.jsx
@@ -1,7 +1,9 @@
 import React, { useMemo } from 'react';
-import { MaterialReactTable } from 'material-react-table';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom'; 
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from 'material-react-table';
+import Button from 'react-bootstrap/Button'; 
 
 const Affichediteurs = ({editeurs, handleDelete }) => {
     const columns = useMemo(
@@ -22,41 +24,50 @@ const Affichediteurs = ({editeurs, handleDelete }) => {
             size: 200,
           },
         
-          {
-            accessorKey: '_id',
-            header: 'Actions',
-            Cell: ({ row }) => (
-              <div>
-                <Link to={`/editeurs/edit/${row.original._id}`}>
-                  <Button variant="warning" size="sm" className="text-warning btn-link edit">
-                    <i className="fa-solid fa-pen-to-square"></i>
-                  </Button>
-                </Link>
-                <Button
-                  variant="danger"
-                  size="sm"
-                  className="text-danger btn-link delete"
-                  onClick={() => handleDelete(row.original._id)}
-                >
-                  <i className="fa fa-trash" />
-                </Button>
-              </div>
-            ),
-          },
-        ],
-        [handleDelete]
-      );
-    
-      return (
-        <MaterialReactTable
-          columns={columns}
-          data={editeurs}
-          enableStickyHeader
-          enableColumnResizing
-        />
-      );
-    
+          { 
+            accessorKey: '_id', 
+            header: 'actions', 
+            size: 100, 
+            Cell: ({ cell, row }) => ( 
+            <div >
+                
+            <Button 
+            onClick={() => { 
+            
+            }} 
+            variant="warning" 
+            size="md" 
+            className="text-warning btn-link edit" 
+            > 
+            <i class="fa-solid fa-pen-to-square"></i> 
+            </Button> 
+            <Button 
+            onClick={(e) => { 
+              deleteProduct(cell.row.original._id,cell.row.original.reference, 
+e); 
+            }} 
+            variant="danger" 
+            size="md" 
+            className="text-danger btn-link delete" 
+            >
+                  <i className="fa fa-trash" /> 
+            </Button> 
+            </div> 
+            ), 
+            },
 
-}
+      
+    ],
+    []
+  );
+
+  const table = useMaterialReactTable({
+    columns,
+    data: editeurs, // data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+  });
+
+  return <MaterialReactTable table={table} />;
+};
+
 
 export default Affichediteurs
